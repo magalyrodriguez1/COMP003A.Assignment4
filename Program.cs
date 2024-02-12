c9d8ad69b3fdbf46db821c5bbe08518d38a986b1
magalyrodriguez1 <142467680+magalyrodriguez1@users.noreply.github.com>
2024-02-11 19:22:00 -0800

    added while loop

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
  * 
  */
 
+using System.Diagnostics.Metrics;
+
 namespace COMP003A.Assignment4
 {
     internal class Program
@@ -44,6 +46,52 @@ namespace COMP003A.Assignment4
             Console.WriteLine("do-while Section");
             Console.WriteLine("Output 0-50 by 5s");
             Console.WriteLine("".PadRight(50, '*'));
+
+            int counter = 0;
+            do
+            {
+                Console.WriteLine(counter);
+                counter += 5; //increments counter by 5
+            }
+            while (counter <= 50); //loops until counter reaches 50. will not go past 50
+
+            Console.WriteLine("\n".PadRight(50, '*'));
+            Console.WriteLine("while Section");
+            Console.WriteLine("Output 1-20");
+            Console.WriteLine("If the number is divisible by 2, output Foo");
+            Console.WriteLine("If the number is divisible by 5, output Bar");
+            Console.WriteLine("If the number is divisible by 2 & 5, output FooBar");
+            Console.WriteLine("".PadRight(50, '*'));
+
+            int counter2 = 1;
+            while (counter2 <= 20)
+            {
+                if (counter2 % 2 == 0 && counter2 % 5 == 0)
+                {
+                    Console.WriteLine("FooBar");
+                    counter2++;
+                }
+                else if (counter2 % 2 == 0)
+                {
+                    Console.WriteLine("Foo");
+                    counter2++;
+                }
+
+                else if(counter2 % 5 == 0)
+                {
+                    Console.WriteLine("Bar");
+                    counter2++;
+                }
+                              
+                else
+                {
+                    Console.WriteLine(counter2);
+                    counter2++;
+
+                }
+            
+
+            }
 
         }
     }
