1b4bbd33a2d43b55b09879328a563a46bb079a6c
magalyrodriguez1 <142467680+magalyrodriguez1@users.noreply.github.com>
2024-02-11 17:10:09 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Author: Magaly Rodriguerz
+ * Purpose: looping statements assignment 4
+ * Course: COMP 003A
+ * 
+ */
+
+namespace COMP003A.Assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("".PadRight(50, '*'));
+            Console.WriteLine("for loop Section");
+            Console.WriteLine("Output a right triangle based on user input");
+            Console.WriteLine("".PadRight(50, '*'));
+
+            Console.WriteLine("Enter a positive whole number:");
+            int limit;
+            limit = Convert.ToInt32(Console.ReadLine());
+
+            string output = "#";
+
+            for (int i = 0; i < limit; i++)
+            {
+                Console.WriteLine(output);
+            output += "#";
+            }
+
+        }
+    }
+}
